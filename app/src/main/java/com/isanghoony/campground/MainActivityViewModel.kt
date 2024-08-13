b2af791a2b30d30d012f9267101236334fab966d
isanghoony <isanghoony@gmail.com>
2024-08-13 23:30:59 +0900

    [Feat] (data): add DI DataModule

--- app/src/main/java/com/isanghoony/campground/MainActivityViewModel.kt
+++ app/src/main/java/com/isanghoony/campground/MainActivityViewModel.kt
@@ -6,16 +6,5 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainActivityViewModel @Inject constructor() : ViewModel() {
-    /*val uiState: StateFlow<MainActivityUiState> = userDataRepository.userData.map {
-        Success(it)
-    }.stateIn(
-        scope = viewModelScope,
-        initialValue = Loading,
-        started = SharingStarted.WhileSubscribed(5_000),
-    )*/
-}
 
-sealed interface MainActivityUiState {
-    data object Loading : MainActivityUiState
-    /*data class Success(val userData: UserData) : MainActivityUiState*/
 }
